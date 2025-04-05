a25167d51cd3b44e0e3b42e16bf895afe4d2cd56
beetosu <cpwalsh@buffalo.edu>
2025-04-04 23:23:01 -0400

    HARD PIVOT! changing the style completely, staring with homepage

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,40 @@
-function transitionColor(color, article) {
-    document.body.className = `transition-${color}`;
-    setTimeout((() => window.location.assign(`/${article}/`)), 250);
+function handleMouseEnter(infoClass) {
+    const infoElements = document.getElementsByClassName(`info ${infoClass}`);
+    for (const element of infoElements) {
+        element.classList.remove('inactive');
+        element.classList.add('active');
+    }
 }
+
+function handleMouseLeave(infoClass) {
+    const infoElements = document.getElementsByClassName(`info ${infoClass}`);
+    for (const element of infoElements) {
+        element.classList.remove('active');
+        element.classList.add('inactive');
+    }
+}
+
+function buildEventListeners(guyId, infoClass) {
+    const guy = document.getElementById(guyId);
+    // Hovering over = active
+    guy.addEventListener('mouseenter', () => {
+        handleMouseEnter(infoClass);
+    });
+    
+    // No longer hovering = inactive
+    guy.addEventListener('mouseleave', () => {
+        handleMouseLeave(infoClass);
+    });
+    
+    // Tabbed in = active
+    guy.addEventListener('focusin', () => {
+        handleMouseEnter(infoClass);
+    })
+    
+    // No longer tabbed in = inactive
+    guy.addEventListener('focusout', () => {
+        handleMouseLeave(infoClass);
+    });
+}
+
+buildEventListeners('about-guy', 'about');
---
+++ scripts/old_index.js
@@ -0,0 +1,4 @@
+function transitionColor(color, article) {
+    document.body.className = `transition-${color}`;
+    setTimeout((() => window.location.assign(`/${article}/`)), 250);
+}
