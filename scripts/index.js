afd0ba67295f88732454eca6504ca38a6ae6635c
beetosu <cpwalsh@buffalo.edu>
2025-04-06 13:14:40 -0400

    add a posts guy

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,49 @@
-function handleMouseEnter(infoClass) {
+function handleMouseEnter(infoClass, sprite, guyElement) {
     const infoElements = document.getElementsByClassName(`info ${infoClass}`);
     for (const element of infoElements) {
         element.classList.remove('inactive');
         element.classList.add('active');
     }
+
+    if (sprite && guyElement) {
+        guyElement.getElementsByTagName('img')[0].src = `assets/${sprite}.gif`;
+    }
 }
 
-function handleMouseLeave(infoClass) {
+function handleMouseLeave(infoClass, sprite, guyElement) {
     const infoElements = document.getElementsByClassName(`info ${infoClass}`);
     for (const element of infoElements) {
         element.classList.remove('active');
         element.classList.add('inactive');
     }
+
+    if (sprite && guyElement) {
+        guyElement.getElementsByTagName('img')[0].src = `assets/${sprite}.gif`;
+    }
 }
 
-function buildEventListeners(guyId, infoClass) {
+function buildEventListeners(guyId, infoClass, activeSprite, inactiveSprite) {
     const guy = document.getElementById(guyId);
     // Hovering over = active
     guy.addEventListener('mouseenter', () => {
-        handleMouseEnter(infoClass);
+        handleMouseEnter(infoClass, activeSprite, guy);
     });
     
     // No longer hovering = inactive
     guy.addEventListener('mouseleave', () => {
-        handleMouseLeave(infoClass);
+        handleMouseLeave(infoClass, inactiveSprite, guy);
     });
     
     // Tabbed in = active
     guy.addEventListener('focusin', () => {
-        handleMouseEnter(infoClass);
+        handleMouseEnter(infoClass, activeSprite, guy);
     })
     
     // No longer tabbed in = inactive
     guy.addEventListener('focusout', () => {
-        handleMouseLeave(infoClass);
+        handleMouseLeave(infoClass, inactiveSprite, guy);
     });
 }
 
 buildEventListeners('about-guy', 'about');
+buildEventListeners('posts-guy', 'posts', 'posts-guy-active', 'posts-guy-inactive');
