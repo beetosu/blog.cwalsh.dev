b8e5da97a539aa1e31f80e19c05978ac2f5dbbc8
beetosu <cpwalsh@buffalo.edu>
2025-04-06 18:25:19 -0400

    add clouds :)

--- scripts/index.js
+++ scripts/index.js
@@ -59,3 +59,32 @@ if (hasTouchSupport) {
     const postGuy = document.getElementById('posts-guy');
     handleMouseEnter('posts', 'posts-guy-active', postGuy);
 }
+
+for (let backgroundIdx = 1; backgroundIdx < 6; backgroundIdx++) {
+    const seed = Math.floor(Math.random() * 10);
+    let backgroundId = `background${backgroundIdx}`;
+
+    const fnSetClassnameToActive = () => document.getElementById(backgroundId).className = 'active';
+
+    /**
+     * Depending on the seed, do the following:
+     * If 9, instantly start active loop.
+     * If 8 or 7, start a "mid active" loop, and set timer to start active loop immedaitely afterwards.
+     * Everything else set timer to start an active loop anywhere between 1 to 100 seconds.
+     */
+    if (seed > 6) {
+        if (seed > 7) {
+            if (seed > 8) {
+                fnSetClassnameToActive();
+            } else {
+                document.getElementById(backgroundId).className =  'mid-active-1';
+                setTimeout(fnSetClassnameToActive, 64000);
+            }
+        } else {
+            document.getElementById(backgroundId).className =  'mid-active-2';
+            setTimeout(fnSetClassnameToActive, 35000);
+        }
+    } else {
+        setTimeout(fnSetClassnameToActive, Math.floor(Math.random() * 25000));
+    }
+}
